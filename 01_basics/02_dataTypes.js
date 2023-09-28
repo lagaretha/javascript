65e7ca9a9f8d513854367a9364e0900d1fde98ae
Akansha Rai <48284707+lagaretha@users.noreply.github.com>
2023-09-28 15:02:34 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+let name = "Akku" //String datatype
+let age = 45 //int datatype
+let isLoggedIn = false // boolean datatype
+let state; //undefined value 
+let temp = null// value is null
+
+// null => stand alone value, 
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof"Akku");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
