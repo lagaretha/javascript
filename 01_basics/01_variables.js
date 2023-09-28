d58834ed3a309bc0cc50137dceb9670d00771a38
Akansha Rai <48284707+lagaretha@users.noreply.github.com>
2023-09-28 14:38:53 +0000

    variables declare

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 145678
+let accountEmail = "akku@google.com"
+var accountPassword = "12345"
+
+accountCity = "Ambootia"
+
+console.log("accountId");
+
+console.table([accountId, accountPassword, accountEmail,accountCity])
