3ebb3cba7c257fa4b30f3063cfb01bb9a92c2b1f
Akansha Rai <48284707+lagaretha@users.noreply.github.com>
2023-09-29 14:02:52 +0000

    Conversions Of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,44 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abs" => NaN not a number
+//true => 1, false => 0
+//null => 0
+//undefined => NaN
+
+let isLoggedIn = "Akkka"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true
+//0 => false
+//"" => falsee
+//"akka" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// Operations 
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+//console.log(2**2); //power 0f 2
+
+let str1 = "Hello"
+let str2 = " Akkansha"
+
+let str3 = str1+str2
+console.log(str3);
+
+
