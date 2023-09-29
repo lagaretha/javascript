f71b95fbbf963069ee29c988d04eed60dc1cf0cd
Akansha Rai <48284707+lagaretha@users.noreply.github.com>
2023-09-29 14:56:22 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);  easy type of comparisions
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+console.log(null > 0); //false
+console.log(null == 0);//false
+console.log(null >= 0);//true  equality check == and comparision work differently <,>=
+                    //comparision convert null to a number, treating it as 0
+
+
+
+
