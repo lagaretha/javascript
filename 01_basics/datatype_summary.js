cf114005bbcd7573b677e544378a0d5062f8d332
Akansha Rai <48284707+lagaretha@users.noreply.github.com>
2023-09-29 15:20:01 +0000

    memory

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,9 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Memory 
+//Primitive data type : String, Big int 
+//Non primitive : Array , Objects , Functions
+
+/// Stack(Primitive) , Heap(Non primitive)
+
+// stack call by value , we get the copy 
+//heap call by reference , we get the refrence 
