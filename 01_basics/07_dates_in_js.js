177215835634944f979854a19672a5fafb003936
Akansha Rai <48284707+lagaretha@users.noreply.github.com>
2023-10-04 10:21:51 +0000

    date

---
+++ 01_basics/07_dates_in_js.js
@@ -0,0 +1,10 @@
+//Date
+
+let myDate = new Date() //type of date is object
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+
+let myTimeStamp = new Date()
+
+console.log(myTimeStamp);
