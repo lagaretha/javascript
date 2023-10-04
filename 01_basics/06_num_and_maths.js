ff683274d0c6bed7108147395922797b640d6296
Akansha Rai <48284707+lagaretha@users.noreply.github.com>
2023-10-04 10:02:47 +0000

    math and num

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,15 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));  // after point two decimal value
+
+const otherNum = 123.6767
+console.log(otherNum.toPrecision(5));
+
+//++++++++++++++++++++++Math+++++++++++++++
+console.log(Math.random());// return values between 0-1
+console.log(((Math.random()*10) + 1)); // multiply by 10 and add 1  to get the number in decimal 
