5e5c06a347fb960400c6e3e902c20c1fea1e9d4b
Akansha Rai <48284707+lagaretha@users.noreply.github.com>
2023-10-04 10:47:58 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,27 @@
+//arrays
+//In java script arrays are resizeable and can obtain mix od diff datatypes 
+//Arrays ca be copied in two ways shallow copy and deep copy
+//Shallow copy: copy whose properties share same references 
+//Deep copy: copy whose properties do not share same references
+
+const myArray = [0,1,2,3,4]
+const name = ["Akke", "Subu", "Shakti"]    
+
+//Arrays Method
+myArray.push(8)
+myArray.push(8)/// arrays can add duplicate values
+console.log(myArray);
+
+myArray.unshift(9)//adds given value at first
+myArray.shift()// remove the elemtent from the first
+
+//slice , splice
+console.log("A", myArray);
+
+const myn1 = myArray.slice(1,3)// slice does't manipulate the ehole array it just slice the given part aof array an print it 
+console.log(myn1);
+console.log(myArray);
+
+const myn2 = myArray.splice(1,3) //splice manipulate the whole array
+console.log("B", myn2);
+console.log(myArray);
