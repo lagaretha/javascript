abbd4ff3d053c8963059e2094c43a0cc11d5fa17
Akansha Rai <48284707+lagaretha@users.noreply.github.com>
2023-10-05 11:07:47 +0000

    objects ans its use

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,18 @@
+const tinderUser = {}
+tinderUser.name = "Subarna"
+tinderUser.age = 20
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const newUser = {
+    isLogged : false,
+    username : {
+        fullname :{
+            firstname : "Shanti",
+            lastname: "Rai"
+        }
+    }
+}
+
+console.log(newUser.username.fullname);
