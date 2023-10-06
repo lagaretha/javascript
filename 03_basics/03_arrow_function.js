af3620a2ddb321f94e26bcb2116f1b0807cf9ee7
Akansha Rai <48284707+lagaretha@users.noreply.github.com>
2023-10-06 10:30:28 +0000

    arrow function

---
+++ 03_basics/03_arrow_function.js
@@ -0,0 +1,18 @@
+const user = {
+    username: "Akansha",
+    age : 23
+    //welcomeMsg: function() {
+      //  console.log(`${this.username}, Welcome to the website`);
+    
+
+}
+
+// function one (){
+//     console.log(this); //in function we use this the output will bw undefined
+// }
+// one()
+
+const two= () => {} //arrow function 
+// const addtwo = (num1 , num2) => num1 + num2 //explicit arrow function 
+const addtwo = (num1 , num2) => (num1 + num2) 
+//if we write code within parethesis then don't write return type 
